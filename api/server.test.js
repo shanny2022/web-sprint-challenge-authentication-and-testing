5d54e621994f6f1653dbb88b6aa3e2441a7b5243
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-28 01:30:33 -0500

    Update server.test.js

--- api/server.test.js
+++ api/server.test.js
@@ -6,20 +6,20 @@ beforeEach(async () => {
   await db.migrate.rollback()
   await db.migrate.latest()
   await request(server)
-    .post('/auth/register')
+    .post('/api/auth/register') // updated here
     .send({
       username: 'tdubs',
       password: '1234'
     })
 })
+
 afterAll(async () => {
   await db.destroy()
 })
 
 describe('Auth Router', () => {
   test('sanity', () => {
-    expect(true)
-      .toBe(true)
+    expect(true).toBe(true)
   })
 
   describe('[POST] /login', () => {
@@ -27,24 +27,22 @@ describe('Auth Router', () => {
 
     test('responds with 200 OK', async () => {
       res = await request(server)
-        .post('/login')
+        .post('/api/auth/login') // updated here
         .send({
           username: 'tdubs',
           password: '1234'
         })
-      console.log(res)
-      expect(res.status)
-        .toBe(200)
+      expect(res.status).toBe(200)
     })
+
     test('responds with 401 invalid credentials', async () => {
       res = await request(server)
-        .post('/login')
+        .post('/api/auth/login') // updated here
         .send({
           username: 'tdu',
           password: '124'
         })
-      expect(res.status)
-        .toBe(401)
+      expect(res.status).toBe(401)
     })
   })
 })
