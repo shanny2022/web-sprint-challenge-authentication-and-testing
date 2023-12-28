7ce41ad06b9ae801e0d0b7894df0c5377513db65
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-28 01:47:30 -0500

    Update verify-hash.js

--- api/middleware/verify-hash.js
+++ api/middleware/verify-hash.js
@@ -1,9 +1,15 @@
 const bcrypt = require('bcryptjs')
 
 module.exports = (req, res, next) => {
-  const {
-    password
-  } = req.user
-  bcrypt.compareSync(req.body.password, password)
+  const { password } = req.user
+  const passwordsMatch = bcrypt.compareSync(req.body.password, password)
+
+  if (!passwordsMatch) {
+    return next({
+      status: 401,
+      message: 'invalid credentials'
+    })
+  }
+
   return next()
 }
